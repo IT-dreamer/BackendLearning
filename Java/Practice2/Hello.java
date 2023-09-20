c9bdba73247e09b568be9656be7faf876b9fa4ac
AFei <tjtgcyf@gmail.com>
2023-09-20 10:31:15 +0900

    new: hello class

---
+++ Java/Practice2/Hello.java
@@ -0,0 +1,24 @@
+/*
+ * @Author: AFei tjtgcyf@gmail.com
+ * @Date: 2023-09-20 09:20:16
+ * @LastEditors: AFei tjtgcyf@gmail.com
+ * @LastEditTime: 2023-09-20 10:30:43
+ * @FilePath: /BackendLearning/Java/Practice2/Hello.java
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by AFei, All Rights Reserved. 
+ */
+package Java.Practice2;
+
+public class Hello
+{
+    /**
+     * @description: main function
+     * @param {String[]} args
+     * @return void
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
