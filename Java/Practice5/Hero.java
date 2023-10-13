d5cbbf3f41013f8a197d6b8b8516aa0e9fbb7a0a
AFei <tjtgcyf@gmail.com>
2023-10-13 16:07:06 +0900

    new: hero class

---
+++ Java/Practice5/Hero.java
@@ -0,0 +1,16 @@
+package Java.Practice5;
+
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+    int deathCount;
+    int killCount;
+    int assistantCount;
+    int money;
+    int kick;
+    String afterKilledSaid;
+    String afterKillSaid;
+
+}
